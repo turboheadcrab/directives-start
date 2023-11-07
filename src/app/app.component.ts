b0e07c4d132c7aaba734bd2b56e2a5d9d804674d
Vladimir Apraksin <vapraksi@utk.edu>
2023-11-07 12:51:37 -0500

    Ported over source code to Angular 16.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'directives-start';
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
